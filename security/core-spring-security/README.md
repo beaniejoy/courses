74c861a6d4876c1261f3ad8c60cd96ba45463599
Hanbin Lee <lhb1025@gmail.com>
2022-10-07 15:13:47 +0900

    스프링 시큐리티 강의 디렉토리 별도 구성

---
+++ security/core-spring-security/README.md
@@ -0,0 +1,4 @@
+# Core Spring Security (Kotlin)
+
+- kotlin, gradle 버전 core-spring-security 강의 내용
+- [강의(스프링 시큐리티 inflearn) repository](https://github.com/onjsdnjs/corespringsecurity)
---
+++ security/core-spring-security/src/test/kotlin/io/beaniejoy/coresecurity/CoreSpringSecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.beaniejoy.coresecurity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoreSpringSecurityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
