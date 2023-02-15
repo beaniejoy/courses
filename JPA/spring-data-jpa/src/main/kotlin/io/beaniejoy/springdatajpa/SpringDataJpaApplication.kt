8a0ab00c3ce92499f55df5004ddc8c8c3d10d013
beaniejoy <lhb1025@gmail.com>
2023-02-15 21:40:27 +0900

    EnableJpaRepositories 내용 추가

--- JPA/spring-data-jpa/src/main/kotlin/io/beaniejoy/springdatajpa/SpringDataJpaApplication.kt
+++ JPA/spring-data-jpa/src/main/kotlin/io/beaniejoy/springdatajpa/SpringDataJpaApplication.kt
@@ -2,8 +2,11 @@ package io.beaniejoy.springdatajpa
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+// SpringBoot 에서는 아래 설정을 자동으로 해준다.
+//@EnableJpaRepositories(basePackages = ["io.beaniejoy.springdatajpa.repository"])
 class SpringDataJpaApplication
 
 fun main(args: Array<String>) {
