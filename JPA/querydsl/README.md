cc6e41de298275531327992bd058c65ea4facf49
beaniejoy <lhb1025@gmail.com>
2023-04-05 21:49:40 +0900

    결과 조회 부분 진행중

--- JPA/querydsl/README.md
+++ JPA/querydsl/README.md
@@ -22,3 +22,45 @@ dependencies {
 implementation("com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.9.0")
 ```
 - 1.9.0 버전에서 Spring Boot 3.x 버전 대응
+
+## :pushpin: 기본 문법
+
+### 기본 QType 활용
+
+```kotlin
+@Test
+fun startQuerydsl() {
+//        val m = QMember("m")
+//        val m = QMember.member
+
+    // static import 간편히 사용 가능
+    val findMember = queryFactory
+        .select(member)
+        .from(member)
+        .where(member.username.eq("member1")) // 파라미터 바인딩
+        .fetchOne()
+
+    assertThat(findMember?.username).isEqualTo("member1")
+}
+```
+- `QMember("m1")`으로 table에 alias 부여가능(같은 테이블 조인할 때만 사용함) 
+- **static import를 사용해서 QType member 사용하는 것을 추천** 
+
+### 검색 조건 쿼리 
+
+```kotlin
+// 위 내용과 같음(","는 and) - 김영한님 추천
+// 조건 중간 null 들어가면 알아서 조건에서 무시
+@Test
+fun searchAndParam() {
+    val member = queryFactory
+        .selectFrom(member)
+        .where(
+            member.username.eq("member1"),
+            member.age.eq(10)
+        )
+        .fetchOne()
+
+    assertThat(member!!.username).isEqualTo("member1")
+}
+```
--- JPA/querydsl/src/test/kotlin/io/beaniejoy/querydsl/QuerydslBasicTest.kt
+++ JPA/querydsl/src/test/kotlin/io/beaniejoy/querydsl/QuerydslBasicTest.kt
@@ -2,7 +2,7 @@ package io.beaniejoy.querydsl
 
 import com.querydsl.jpa.impl.JPAQueryFactory
 import io.beaniejoy.querydsl.entity.Member
-import io.beaniejoy.querydsl.entity.QMember
+import io.beaniejoy.querydsl.entity.QMember.member
 import io.beaniejoy.querydsl.entity.Team
 import jakarta.persistence.EntityManager
 import jakarta.transaction.Transactional
@@ -15,20 +15,26 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 @Transactional
 class QuerydslBasicTest {
+    // 멀티스레드 환경에서도 동시성 이슈 없이 작동되도록 주입시켜줌
+    // @Transactional 걸린 부분에 따라 처리 되도록 설계됨
     @Autowired
     lateinit var em: EntityManager
 
+    lateinit var queryFactory: JPAQueryFactory
+
     @BeforeEach
     fun before() {
+        queryFactory = JPAQueryFactory(em)
+
         val teamA = Team.createTeam("teamA")
         val teamB = Team.createTeam("teamB")
         em.persist(teamA)
         em.persist(teamB)
 
         val member1 = Member.createMember("member1", 10).apply { this.changeTeam(teamA) }
-        val member2 = Member.createMember("member2", 10).apply { this.changeTeam(teamA) }
+        val member2 = Member.createMember("member2", 20).apply { this.changeTeam(teamA) }
         val member3 = Member.createMember("member3", 10).apply { this.changeTeam(teamB) }
-        val member4 = Member.createMember("member4", 10).apply { this.changeTeam(teamB) }
+        val member4 = Member.createMember("member4", 20).apply { this.changeTeam(teamB) }
         em.persist(member1)
         em.persist(member2)
         em.persist(member3)
@@ -50,15 +56,55 @@ class QuerydslBasicTest {
 
     @Test
     fun startQuerydsl() {
-        val queryFactory = JPAQueryFactory(em)
-        val m = QMember("m")
+//        val m = QMember("m1")
+//        val m = QMember.member
 
+        // static import 간편히 사용 가능
         val findMember = queryFactory
-            .select(m)
-            .from(m)
-            .where(m.username.eq("member1"))
+            .select(member)
+            .from(member)
+            .where(member.username.eq("member1")) // 파라미터 바인딩
             .fetchOne()
 
         assertThat(findMember?.username).isEqualTo("member1")
+    }
+
+    @Test
+    fun search() {
+        val member = queryFactory
+            .selectFrom(member)
+            .where(
+                member.username.eq("member1")
+                    .and(member.age.eq(10))
+            )
+            .fetchOne()
+
+        assertThat(member!!.username).isEqualTo("member1")
+    }
+
+    // 위 내용과 같음(","는 and) - 김영한님 추천
+    // 중간에 null 들어가면 알아서 조건에서 무시
+    @Test
+    fun searchAndParam() {
+        val member = queryFactory
+            .selectFrom(member)
+            .where(
+                member.username.eq("member1"),
+                member.age.eq(10)
+            )
+            .fetchOne()
+
+        assertThat(member!!.username).isEqualTo("member1")
+    }
+
+    @Test
+    fun resultFetch() {
+        val list: List<Member> = queryFactory
+            .selectFrom(member)
+            .fetch()
+
+        val one: Member? = queryFactory
+            .selectFrom(member)
+            .fetchOne()
     }
 }
